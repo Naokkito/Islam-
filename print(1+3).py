52d5aeb72ca7689e7a58a71e2e359904747fbe5b
Naokkito <144862462+Naokkito@users.noreply.github.com>
2023-09-19 15:55:02 +0900

    is.py

---
+++ print(1+3).py
@@ -0,0 +1,38 @@
+#a = float(input(" work hours:")) 
+#b = float(input("salary rate:"))
+#print("salary:" + str(a*b) + "$")
+
+#c = float(input("Celsius:"))
+#b = c * 9 / 5 + 32
+#print("Fahrenheit:" + str(b))
+
+#s = int(input("enter seconds:"))
+#n = s/3600 
+#c = n%3600
+#f = c/60
+#d = c%60
+
+#print(str(s) + " seconds is " + str(n)  + " hours " + str(c) + " minutes " + str(d) + " s " )
+
+"""#print(int(3.141595))
+#print(float(365))
+#print(str(50))
+#print(float(99) + 100)
+#my_string = "hello" + "there"
+#my_string = my_string + str(1)
+#print(my_string)
+age = input(“Enter your age : ”)
+print(type(age))
+age = int(input(“Enter your age : ”))
+print(type(age))"""
+
+s = input("Enter your name:")
+print("hello " + s)
+
+
+
+
+
+
+
+
