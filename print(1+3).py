076829f37e8144e7844a20535af7ae164e63213a
Naokkito <144862462+Naokkito@users.noreply.github.com>
2023-09-19 15:56:15 +0900

    Update print(1+3).py

--- print(1+3).py
+++ print(1+3).py
@@ -14,20 +14,6 @@
 
 #print(str(s) + " seconds is " + str(n)  + " hours " + str(c) + " minutes " + str(d) + " s " )
 
-"""#print(int(3.141595))
-#print(float(365))
-#print(str(50))
-#print(float(99) + 100)
-#my_string = "hello" + "there"
-#my_string = my_string + str(1)
-#print(my_string)
-age = input(“Enter your age : ”)
-print(type(age))
-age = int(input(“Enter your age : ”))
-print(type(age))"""
-
-s = input("Enter your name:")
-print("hello " + s)
 
 
 
